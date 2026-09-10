01918e8419eb2f19abbc850b175758142ba047ea
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2026-09-10 16:05:36 -0500

    Improve error handling

--- template/runbook/backupmysql.ps1
+++ template/runbook/backupmysql.ps1
@@ -52,7 +52,6 @@ Write-Output "Retrieved MySQL credential"
 
 # Construct the container entry command
 [string]$BackupJobTimeStamp = Get-Date -Format "yyyyMMddhhmmss"
-# LATER: Allow customizing file name prefix
 [string]$filename = "--result-file=/data/backups/" + $BackupFileNamePrefix + $BackupJobTimeStamp + ".sql"
 [string]$HostName = "--host=$DatabaseHostName"
 [string]$user = "--user=$MySQLUsername"
@@ -68,46 +67,46 @@ foreach ($DatabaseName in $DatabaseNamesArray) {
     $cmd += $DatabaseName
 }
 
-# Get storage account access key
-[securestring]$StorageAccountKey = ConvertTo-SecureString ((Get-AzStorageAccountKey -ResourceGroupName $ContainerResourceGroupName -AccountName $StorageAccountName) `
-    | Where-object { $_.KeyName -eq "Key1" }).Value -AsPlainText -Force
-
-# Create mount object as backup volume in container
-$VolumeMount = New-AzContainerInstanceVolumeMountObject -Name "backups" -MountPath "/data/backups/" -ReadOnly $false
-# Create a new volume on the mount object from the Azure File share
-$Volume = New-AzContainerGroupVolumeObject -Name "backups" -AzureFileShareName $BackupFileShareName `
-    -AzureFileStorageAccountName $StorageAccountName `
-    -AzureFileStorageAccountKey $StorageAccountKey 
-
-$ContainerRegistryCredential = Get-AutomationPSCredential -Name "ContainerRegistryCredential"
-$ContainerRegistryUsername = $ContainerRegistryCredential.UserName
-$ContainerRegistryPassword = ConvertTo-SecureString ($ContainerRegistryCredential.GetNetworkCredential().Password) -AsPlainText -Force
-$ImageRegistryCredential = New-AzContainerGroupImageRegistryCredentialObject -Server $ContainerRegistryUrl -Username $ContainerRegistryUsername -Password $ContainerRegistryPassword
-
-$EnvironmentVariables = @(
-    (New-AzContainerInstanceEnvironmentVariableObject -Name "STORAGE_ACCOUNT_NAME" -Value $StorageAccountName),
-    (New-AzContainerInstanceEnvironmentVariableObject -Name "BLOB_CONTAINER_NAME" -Value $BackupBlobContainerName),
-    # Log folder does not need to exist yet
-    (New-AzContainerInstanceEnvironmentVariableObject -Name "AZCOPY_LOG_LOCATION" -Value "/data/backups/azcopy-logs"),
-    (New-AzContainerInstanceEnvironmentVariableObject -Name "AZCOPY_AUTO_LOGIN_TYPE" -Value "MSI"),
-    (New-AzContainerInstanceEnvironmentVariableObject -Name "AZCOPY_MSI_CLIENT_ID" -Value $ManagedIdentityClientId),
-    (New-AzContainerInstanceEnvironmentVariableObject -Name "MYSQL_PASSWORD" -SecureValue $MySQLPassword)
-)
-
-# Create the container instance object
-$Container = New-AzContainerInstanceObject -Name $ContainerName -Image "$ContainerRegistryUrl/azure-mysql-ltr/mysql-ltr-dump:latest" -VolumeMount $VolumeMount `
-    -Command $cmd -EnvironmentVariable $EnvironmentVariables `
-    -RequestCpu $ContainerCpuCores -RequestMemoryInGb $ContainerMemoryInGb
-
-$SubnetId = @{
-    Id   = $ContainerInstanceSubnetResourceId
-    Name = "ContainerSubnet"   
-}
-
-$ContainerGroupIdentity = @{}
-$ContainerGroupIdentity[$ManagedIdentityResourceId] = @{}
-
 try {
+    # Get storage account access key
+    [securestring]$StorageAccountKey = ConvertTo-SecureString ((Get-AzStorageAccountKey -ResourceGroupName $ContainerResourceGroupName -AccountName $StorageAccountName) `
+        | Where-object { $_.KeyName -eq "Key1" }).Value -AsPlainText -Force
+
+    # Create mount object as backup volume in container
+    $VolumeMount = New-AzContainerInstanceVolumeMountObject -Name "backups" -MountPath "/data/backups/" -ReadOnly $false
+    # Create a new volume on the mount object from the Azure File share
+    $Volume = New-AzContainerGroupVolumeObject -Name "backups" -AzureFileShareName $BackupFileShareName `
+        -AzureFileStorageAccountName $StorageAccountName `
+        -AzureFileStorageAccountKey $StorageAccountKey 
+
+    $ContainerRegistryCredential = Get-AutomationPSCredential -Name "ContainerRegistryCredential"
+    $ContainerRegistryUsername = $ContainerRegistryCredential.UserName
+    $ContainerRegistryPassword = ConvertTo-SecureString ($ContainerRegistryCredential.GetNetworkCredential().Password) -AsPlainText -Force
+    $ImageRegistryCredential = New-AzContainerGroupImageRegistryCredentialObject -Server $ContainerRegistryUrl -Username $ContainerRegistryUsername -Password $ContainerRegistryPassword
+
+    $EnvironmentVariables = @(
+        (New-AzContainerInstanceEnvironmentVariableObject -Name "STORAGE_ACCOUNT_NAME" -Value $StorageAccountName),
+        (New-AzContainerInstanceEnvironmentVariableObject -Name "BLOB_CONTAINER_NAME" -Value $BackupBlobContainerName),
+        # Log folder does not need to exist yet
+        (New-AzContainerInstanceEnvironmentVariableObject -Name "AZCOPY_LOG_LOCATION" -Value "/data/backups/azcopy-logs"),
+        (New-AzContainerInstanceEnvironmentVariableObject -Name "AZCOPY_AUTO_LOGIN_TYPE" -Value "MSI"),
+        (New-AzContainerInstanceEnvironmentVariableObject -Name "AZCOPY_MSI_CLIENT_ID" -Value $ManagedIdentityClientId),
+        (New-AzContainerInstanceEnvironmentVariableObject -Name "MYSQL_PASSWORD" -SecureValue $MySQLPassword)
+    )
+
+    # Create the container instance object
+    $Container = New-AzContainerInstanceObject -Name $ContainerName -Image "$ContainerRegistryUrl/azure-mysql-ltr/mysql-ltr-dump:latest" -VolumeMount $VolumeMount `
+        -Command $cmd -EnvironmentVariable $EnvironmentVariables `
+        -RequestCpu $ContainerCpuCores -RequestMemoryInGb $ContainerMemoryInGb
+
+    $SubnetId = @{
+        Id   = $ContainerInstanceSubnetResourceId
+        Name = "ContainerSubnet"   
+    }
+
+    $ContainerGroupIdentity = @{}
+    $ContainerGroupIdentity[$ManagedIdentityResourceId] = @{}
+
     # Deploy the container in a container group
     Write-Output "Creating container..."
     $ContainerGroup = New-AzContainerGroup -ResourceGroupName $ContainerResourceGroupName -Name $ContainerName `
@@ -117,7 +116,7 @@ try {
         -IdentityType "UserAssigned" -IdentityUserAssignedIdentity $ContainerGroupIdentity
 
     while ($true) {
-        $Status = (Get-AzContainerGroup -Name $ContainerName -ResourceGroupName $ContainerResourceGroupName | Select-Object -Property @{Name = "Status"; Expression = { $_.InstanceViewState } }).Status
+        $Status = (Get-AzContainerGroup -Name $ContainerGroup.Name -ResourceGroupName $ContainerResourceGroupName | Select-Object -Property @{Name = "Status"; Expression = { $_.InstanceViewState } }).Status
 
         if ($Status -eq "Failed") {
             Write-Error "Container in Failed State. Please check the logs below."
@@ -144,13 +143,15 @@ catch {
     throw
 }
 finally {
-    [string]$separator = '=' * 80
-    Write-Output "Fetching container logs..."
-    Write-Output $separator
-    Get-AzContainerInstanceLog -ContainerGroupName $ContainerGroup.Name -ContainerName $ContainerName -ResourceGroupName $ContainerResourceGroupName | Write-Output
-    Write-Output $separator
+    if ($null -ne $ContainerGroup) {
+        [string]$separator = '=' * 80
+        Write-Output "Fetching container logs..."
+        Write-Output $separator
+        Get-AzContainerInstanceLog -ContainerGroupName $ContainerGroup.Name -ContainerName $ContainerName -ResourceGroupName $ContainerResourceGroupName | Write-Output
+        Write-Output $separator
 
-    # Stop container after backup
-    Write-Output "Stopping container..."
-    Stop-AzContainerGroup -Name $ContainerGroup.Name -ResourceGroupName $ContainerResourceGroupName
+        # Stop container after backup
+        Write-Output "Stopping container..."
+        Stop-AzContainerGroup -Name $ContainerGroup.Name -ResourceGroupName $ContainerResourceGroupName
+    }
 }
